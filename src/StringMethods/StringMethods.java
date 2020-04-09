a39bb4274014defcaa74a932e237f8fb14bcfd9c
Sofia Velarde <sofiavelarde2004@gmail.com>
2020-04-08 19:55:23 -0700

    StringMethods (IP)
    Left off on 3rd method

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,12 +32,21 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		int compare = s1.compareTo(s2);
+		if (s1.length() > s2.length()) {
+			return s1;
+		}else {
+			return s2;
+		}
+	
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
+		if (s.contains("underscores")) {
+			s.replace(" ", "_");
+		}
 		return null;
 	}
 
